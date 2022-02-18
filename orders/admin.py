dd8aac2be13535645436db7a4b2e0b8079d39eaa
akashojha02 <akashojha.u@gmail.com>
2022-02-18 20:55:57 +0530

    password

---
+++ orders/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Order,Payment,OrderProduct
+
+
+admin.site.register(Order)
+admin.site.register(Payment)
+admin.site.register(OrderProduct)
+# Register your models here.
